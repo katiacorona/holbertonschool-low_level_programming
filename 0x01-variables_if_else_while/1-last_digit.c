944551ac87588c63dfe59ac7ad05c09c3d4f4788
katiacorona <katiacorona@gmail.com>
2021-10-01 16:14:28 -0700

    Assigns random number to n and prints the last digit stored in n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+/**
+ * File: 1-last_digit.c
+ * Auth: Katia Corona
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point Assigns random number to n and prints the last digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int s;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	s = n % 10;
+	if (s > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, s);
+	}
+	else if (s == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, s);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
+	}
+	return (0);
+}
