d64de388d0a86d048056c4d0697fdd4dc0b32c11
katiacorona <katiacorona@gmail.com>
2021-10-07 11:56:34 -0700

    Removes _putchar.h from main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * main - prints _putchar exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char output[9] = "_putchar\n";
+
+	int i;
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(output[i]);
+	}
+
+	return (0);
+}
