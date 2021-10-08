b3df8433348cd4922ccfbc2e1230437e946ae5fe
katiacorona <katiacorona@gmail.com>
2021-10-08 12:55:56 -0700

    Includes the prototype and function definition for task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-islower.c
+ * Auth: Katia C
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - returns 1 when lowercase, and 0 otherwise.
+ *
+ * c - takes the value in ascii for an int type.
+ *
+ * return: Always 0 (Successful)
+ */
+int _islower(int c)
+{
+	int c;
+
+	if ((c >= 97) && (c <= 122))
+	{
+		_putchar(1);
+	}
+	else if ((c >= 65) && (c <= 90))
+	{
+		_putchar(0)
+	}
+	return (0);
+}
