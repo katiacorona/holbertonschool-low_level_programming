e0d4564ca5c29b922dd0c325c6da6f107b2914b9
katiacorona <katiacorona@gmail.com>
2021-10-08 13:20:17 -0700

    Test to correct the no return value description error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,20 +6,16 @@
 #include "main.h"
 
 /**
- * _islower - returns 1 when lowercase, and 0 otherwise.
- *
- * @c: takes the character for review.
+ * _islower - returns 1 when lowercase and 0 otherwise.
+ * @c: takes the character to be evaluated.
  *
  * return: 1 if lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
 	if ((c >= 'a') && (c <= 'z'))
-	{
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
