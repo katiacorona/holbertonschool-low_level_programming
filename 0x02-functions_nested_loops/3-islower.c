82d1e3aa6a70ab1c253bdf89d9ef5ec3aaf66253
katiacorona <katiacorona@gmail.com>
2021-10-08 13:01:34 -0700

    Removes redeclarations of c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,15 +14,13 @@
  */
 int _islower(int c)
 {
-	int c;
-
 	if ((c >= 97) && (c <= 122))
 	{
 		_putchar(1);
 	}
 	else if ((c >= 65) && (c <= 90))
 	{
-		_putchar(0)
+		_putchar(0);
 	}
 	return (0);
 }
