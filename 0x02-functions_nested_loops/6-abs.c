51ef79530e91f4103662e3fa929c2c52d4f2e236
katiacorona <katiacorona@gmail.com>
2021-10-08 16:56:43 -0700

    First test to print the absolute of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs: Computes the absolute value of an int.
+ *
+ * Return: Always 0 (Success).
+ */
+int _abs(int)
+{
+	int i;
+	
+	for (i >= 0)
+		return (i);
+	
+	else
+		return (-i);
+}
