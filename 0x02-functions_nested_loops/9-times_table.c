cd25767cb85a160fb4c292139f4baa50337939e9
katiacorona <katiacorona@gmail.com>
2021-10-09 09:14:41 -0700

    Includes the function declaration of task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0.
+ */
+void times_table(void)
+{
+	int f1, f2, prod;
+
+	for (f1 = 0; f1 <= 9; f1++)
+	{
+		_putchar('0');
+
+		for (f2 = 1; f2 <= 9; f2++)
+		{
+			prod = (f1 * f2);
+
+			_putchar(',');
+			_putchar(' ');
+
+			if (prod < 10)
+				_putchar(' ');
+
+			_putchar(prod);
+			_putchar('\n');
+		}
+	}
+}
