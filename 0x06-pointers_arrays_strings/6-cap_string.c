63be5d420ba05915fc68832d822ff69673d87cec
katiacorona <katiacorona@gmail.com>
2021-10-15 19:42:05 -0700

    Second test

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,9 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	char separator[n];
+	int i = 0, n;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	separator[] = " \t\n,;.!?\"(){}";
 	while (str[i])
 	{
 		while ((str[i] >= 'a') && (str[i] <= 'z'))
@@ -20,9 +19,13 @@ char *cap_string(char *str)
 			i++;
 		}
 
-		if (str[i--] == separator[n])
+		for (n = 0; n <= 13; n++)
 		{
-			str[i] -= 32;
+
+			if ((str[i--] == sep[n]) || (i == 0))
+			{
+				str[i] -= 32;
+			}
 		}
 		i++;
 	}
