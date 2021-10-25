f15369d9e3cde40ea56f902a086c8b44f3a30959
katiacorona <katiacorona@gmail.com>
2021-10-25 16:40:03 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest: memory area where n bytes will be copied to
+ * @src: memory area where n were bytes will be copied from
+ * @n: number of bytes to be copied
+ * Return: a pointer to 'dest'
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	unsigned char *ptrDest = dest;
+	unsigned char *ptrSrc = src;
+
+	for (i = 0; i < n; i++)i
+	{
+		ptrDest[i] = ptrSrc[i];
+	}
+	return (dest);
+}
