81e5c77701808fc7b7dcf534bcd1fd90181d3422
katiacorona <katiacorona@gmail.com>
2021-10-25 17:36:28 -0700

    First test for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string where @c will be located
+ * @c: the character to be located
+ * Return: a pointer to the first occurance;
+ *		if not found, returns NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] =! '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+	}
+	return ('\0');
+}
