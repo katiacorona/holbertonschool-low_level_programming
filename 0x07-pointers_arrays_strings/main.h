bf4daa9e6bcd64ac9441ecc7411442a37e9c3637
katiacorona <katiacorona@gmail.com>
2021-10-28 18:05:44 -0700

    Includes task 3

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int _putchar(char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n);
+
+unsigned int _strspn(char *s, char *accept);
