afde189d8a27c928adb3e7ccc8a43e35b3e8cc8d
katiacorona <katiacorona@gmail.com>
2021-11-02 20:15:32 -0700

    Using integers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(n);
+		_putchar((n % 10) + '0');
 	}
 	_putchar('\n');
 }
