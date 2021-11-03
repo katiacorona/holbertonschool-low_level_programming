086202a9b880bb34f265b59f39ea24e48024777c
katiacorona <katiacorona@gmail.com>
2021-11-02 19:50:45 -0700

    Second test for function 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,12 @@
  */
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i >= '0' && i <= '9')
 	{
-		_putchar("%d\n", i);
+		_putchar(i);
+		_putchar('\n');
+		i++;
 	}
 }
