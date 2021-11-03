f065b9891b0184e7b7d049bcf2b0efd406e5553f
katiacorona <katiacorona@gmail.com>
2021-11-02 19:33:35 -0700

    Includes the function for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - mulpiplies two integers
+ * @a: takes the multiplicand
+ * @b: takes the multiplier
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = (a * b);
+	return (0);
+}
