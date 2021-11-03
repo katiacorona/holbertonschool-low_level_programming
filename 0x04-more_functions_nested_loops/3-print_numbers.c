7e75aad5f068748daff7eb9896593cb16b273c6b
katiacorona <katiacorona@gmail.com>
2021-11-02 19:56:42 -0700

    added single quotes to integers to be printed with _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,11 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i >= '0' && i <= '9')
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
-		_putchar('\n');
-		i++;
 	}
-	return (i);
+	_putchar('\n');
 }
