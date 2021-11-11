77b476a9c6c2c4b7793d298a593be8c9f6b80b0e
katiacorona <katiacorona@gmail.com>
2021-11-11 21:39:11 +0000

    Includes the missing user library

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - returns the sum of all its parameters
@@ -10,6 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 	va_list list;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
