1fd3ccdfab54f0f91bc7d0daed141958c6eb8810
katiacorona <katiacorona@gmail.com>
2021-11-17 23:41:04 +0000

    removes the if to pass betty and adds the unused attribute

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,8 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
-	if (argc)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
