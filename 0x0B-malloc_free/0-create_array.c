515cb33d7f0d87f103a8eb865f251429281f73bd
katiacorona <katiacorona@gmail.com>
2021-11-20 04:12:07 +0000

    fixes an argument with ==

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	a = malloc(sizeof(char) * size);
+	if (a == 0)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
