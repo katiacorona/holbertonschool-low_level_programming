ac8a504fd8423a449fbdababe9fe3285678b30e3
katiacorona <katiacorona@gmail.com>
2021-11-23 02:53:49 +0000

    tries to fix check 9 with a condition for len == -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
-	while (text_content[len] != '\0')
+	if (text_content != NULL)
 	{
-		len++;
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
 	}
 	o = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0600);
 	w = write(o, text_content, len);
 
-	if (o == -1 || w == -1 || filename == NULL)
+	if (o == -1 || w == -1 || filename == NULL | len == -1)
 	{
 		return (-1);
 	}
