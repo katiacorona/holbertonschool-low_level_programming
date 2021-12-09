891f7de53fcbb6da278e3bf1f671e41cd5fb41fc
katiacorona <katiacorona@gmail.com>
2021-12-09 05:29:52 +0000

    sets memory to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *alloc;
+	char *alloc;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	alloc = malloc(sizeof(nmemb) * size);
+	alloc = malloc(nmemb * size);
 	if (alloc == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		alloc[i] = 0;
 	}
 	return (alloc);
 }
