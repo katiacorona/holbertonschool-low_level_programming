a9b974ee7b389474c2017088e8ca7e7163bd0771
katiacorona <katiacorona@gmail.com>
2022-01-07 19:45:35 +0000

    First

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,15 +13,14 @@
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
  * dog_t - new typedef for struct dog.
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
