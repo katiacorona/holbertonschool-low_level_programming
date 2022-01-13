30d6e2f7de9035e2cd99b6c4dc903da9835c05ce
katiacorona <katiacorona@gmail.com>
2022-01-13 04:08:34 +0000

    Fix:
    retval -1 for array == NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp == NULL || size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
-	return (0);
+	return (-1);
 }
