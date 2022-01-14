1a6d0d234820ffd6ad5c655ca6fa5aee941a5668
katiacorona <katiacorona@gmail.com>
2022-01-14 07:25:31 +0000

    Includes:
    Source code for a program that takes two ints and one operator and returns the result of the operation between the ints

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform an operation.
+ *
+ * @s: a pointer to the operator passed as argument.
+ *
+ * Return: a pointer to the correct function;
+ *			NULL if s does not match any operator.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL && strcmp(s, ops[i].op))
+	{
+		i++;
+	}
+
+	return (ops[i].f);
+}
