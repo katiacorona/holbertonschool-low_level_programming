76f2fd7e858b88558347d9378dfce21248fbcceb
katiacorona <katiacorona@gmail.com>
2022-02-02 01:37:54 +0000

    Fixes:
    trailing whitespace warnings from betty_style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL) 
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		else 
+		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
 		}
