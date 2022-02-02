337fe8706df33912596e6a01714d2a34baf6028e
katiacorona <katiacorona@gmail.com>
2022-02-02 03:55:38 +0000

    Third test

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,10 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,13 +24,9 @@ typedef struct list_s
 /* Prototypes */
 
 size_t print_list(const list_t *h);
-
 size_t list_len(const list_t *h);
-
 list_t *add_node(list_t **head, const char *str);
-
 list_t *add_node_end(list_t **head, const char *str);
-
 void free_list(list_t *head);
 
 #endif /* LISTS_H */
