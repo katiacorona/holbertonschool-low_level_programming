33251a5aa5164d2839b8c0321e2ad681ea528fb8
katiacorona <katiacorona@gmail.com>
2022-02-03 02:06:07 +0000

    Includes:
    function definition for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t and sets the head to NULL.
+ *
+ * @head: a double pointer to the first element of the list.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
+}
