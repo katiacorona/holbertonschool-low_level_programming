25de7a9123432e82c4acd9ff9d8bf2cdddf304b3
katiacorona <katiacorona@gmail.com>
2022-02-03 03:24:10 +0000

    Fixes:
    retval for head == NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *ptr;
 
 	ptr = head;
+
+	if (head == NULL)
+		return (NULL);
+
 	for (count = 0; count <= index; count++)
 	{
 		ptr = ptr->next;
