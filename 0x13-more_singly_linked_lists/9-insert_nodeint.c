21748d9b6fb9977ca1ea6d485e73cd9321365e47
katiacorona <katiacorona@gmail.com>
2022-02-04 02:57:34 +0000

    Test 4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,10 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (node = 0; node < (idx - 1); node++)
 	{
-		ptr = ptr->next;
-
 		if (ptr == NULL || ptr->next == NULL)
 			return (NULL);
+
+		ptr = ptr->next;
 	}
 
 	new->next = ptr->next;
