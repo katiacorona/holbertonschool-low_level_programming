cf9e23e89d2bf722a6c289003808ed97d2cdd595
katiacorona <katiacorona@gmail.com>
2022-02-17 01:27:27 +0000

    Another alternative

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,12 +16,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-		{
+		while (text_content[len])
 			len++;
-		}
 	}
-	o = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0600);
+
+	o = open(filename, O_TRUNC | O_RDWR | O_CREAT, S_IRUSR ^ S_IWUSR);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1 || filename == NULL || len == -1)
