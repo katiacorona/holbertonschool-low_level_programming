d9aab45b0bf0f88f7e4f2ac0d9006cebb0974279
katiacorona <katiacorona@gmail.com>
2022-03-25 01:24:17 +0000

    fixes kernel documentation style!

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,24 +3,14 @@
 
 #include <stddef.h>
 
-/**
- * Directory 0x14. C - Bit Manipulation
- * Contents:	Includes function prototypes for tasks 0 to 5,
- *		including _putchar function.
- */
+/* function prototypes */
 
 unsigned int binary_to_uint(const char *b);
-
 void print_binary(unsigned long int n);
-
 int get_bit(unsigned long int n, unsigned int index);
-
 int set_bit(unsigned long int *n, unsigned int index);
-
 int clear_bit(unsigned long int *n, unsigned int index);
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
 int _putchar(char c);
 
 #endif /* MAIN_H */
