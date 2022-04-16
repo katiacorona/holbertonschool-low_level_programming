fdb16270cdff47866b74d6e1ccb6ca046bfbadb0
katiacorona <katiacorona@gmail.com>
2022-04-16 06:42:45 +0000

    Very first working version of the shell

--- 0x16-simple_shell/1-super_simple_sh.c
+++ 0x16-simple_shell/1-super_simple_sh.c
@@ -1,25 +1,14 @@
-/**
- * 4/13/2022
- * Notes: Managed to run the super simple shell, prints a prompt, reads a line
- * which is the path of the command to be run, and tries to execute it, altough
- * the problem seems to be the null terminating character at the end of the
- * line. After solving this, what's next is creating a child process that will
- * execute the provided command in the cwd. (Use a tokenizer function for remo-
- * ving the new line character and adding the null terminated character to the
- * command.)
- */
-
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 
-#define UNUSED(x) (void)(x)
+int _strlen(char *s);
+char *create_token(char *s);
 
 /**
- * _strlen - Calculates the lenght of a string.
+ * _strlen - Takes a string as input and returns the lenght.
  *
  * @s: A pointer to an array of characters.
  *
@@ -29,14 +18,37 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while (s[len])
+	while (s[len] != '\0')
 		len++;
 	return (len);
 }
 
 /**
+ * create_token - Takes a string as input and returns a token.
  *
+ * @s: The string passed to the function.
+ *
+ * Return: The newly created token.
  */
+char *create_token(char *s)
+{
+	int len = _strlen(s), i = 0;
+	char *token;
+
+	token = malloc(len * sizeof(char));
+	if (token == NULL)
+	{
+		perror("Error: Unable to allocate memory\n");
+		exit(1);
+	}
+	while (s[i] != '\n')
+	{
+		token[i] = s[i];
+		i++;
+	}
+	token[i] = '\0';
+	return (token);
+}
 
 /**
  * main - a first version of a super simple shell.
@@ -48,8 +60,8 @@ int _strlen(char *s)
  */
 int main(void)
 {
-/*	pid_t child_pid;
-	int status;*/
+	pid_t child_pid;
+	int status;
 	size_t bufsize = 0;
 	char *line = NULL;
 	char *args[] = {NULL, NULL, NULL};
@@ -63,12 +75,25 @@ int main(void)
 			exit(1);
 		}
 		printf("%s", line);
-		args[0] = line;
-		if ((execve(args[0], args, NULL)) == -1)
+		args[0] = create_token(line);
+		child_pid = fork();
+		if (child_pid == -1)
 		{
-        		perror("Error: Executing in child\n");
+			perror("Error: Executing in child\n");
 			return (1);
 		}
+		else if (child_pid == 0)
+		{
+			if ((execve(args[0], args, NULL)) == -1)
+			{
+				perror("Error: Executing in child\n");
+				return (1);
+			}
+		}
+		else
+		{
+			sleep(1);
+			wait(&status);
+		}
 	}
-	return (0);
 }
