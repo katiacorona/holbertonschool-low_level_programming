6f7669030ac7c154fbf1f5dce0ef5ae076f2a8db
katiacorona <katiacorona@gmail.com>
2022-05-05 22:18:41 +0000

    Includes: free doubly list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ *
+ * @head: A pointer to the head node of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
