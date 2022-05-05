cde8c6c8a34e618820d4d5c0705162a8a2568e70
Katia <69087487+katiacorona@users.noreply.github.com>
2022-05-05 12:30:31 -0500

    Update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,9 +28,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-<<<<<<< HEAD
-=======
+
 void myStartupFunc(void) __attribute__ ((constructor));
->>>>>>> f983bccc5d5d7b34571879f7d00595cdc6ed132b
 
 #endif /* LISTS_H */
