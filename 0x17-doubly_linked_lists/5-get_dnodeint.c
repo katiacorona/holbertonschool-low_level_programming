d72c0275d623f3b59db2faf8748177cb041216cc
katiacorona <katiacorona@gmail.com>
2022-05-05 22:39:46 +0000

    Includes: case for index out of range

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	for (; index != 0; index--)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
 	return (head);
