0d9356e09714ad3b630015da67e5db6dfdac963e
katiacorona <katiacorona@gmail.com>
2022-05-10 03:05:13 +0000

    Remaining

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - mulpiplies two integers
+ * @a: takes the multiplicand
+ * @b: takes the multiplier
+ * Return: p, the product of the multiplication
+ */
+int mul(int a, int b)
+{
+	int p;
+
+	p = (a * b);
+	return (p);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		if ((n != 2) && (n != 4))
+		{
+			_putchar((n % 10) + '0');
+			n++;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	_putchar('\n');
+}
